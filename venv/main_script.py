55bdb6503124356193f10a155177d9ac4218a1b4
Vedran <carobnjak.best@gmail.com>
2023-10-29 00:11:57 +0100

    second commit

--- venv/main_script.py
+++ venv/main_script.py
@@ -1,12 +1,12 @@
 
 
 
-world size
+world_size = 10
 
-World_list_x_y = [i for i in range(10)]
+World_list_x_y = [i for i in range(world_size)]
 
-for i in range(10):
-    World_list_x_y[i] = [j for j in range(10)] #
+for i in range(world_size):
+    World_list_x_y[i] = [j for j in range(world_size)] #
 
 print(World_list_x_y[4][7])
 class Agent:
