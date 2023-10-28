480b37d216f678b95956735e7070e3409c5a3f30
Vedran <carobnjak.best@gmail.com>
2023-10-29 00:06:15 +0100

    initial commit im losing my mind

--- venv/main_script.py
+++ venv/main_script.py
@@ -1,9 +1,13 @@
 
-World = [i for i in range(10)]
-#aw3454
-for i in range(10):
-    World[i] = [j for j in range(10)] #
 
-print(World[4][7])
+
+world size
+
+World_list_x_y = [i for i in range(10)]
+
+for i in range(10):
+    World_list_x_y[i] = [j for j in range(10)] #
+
+print(World_list_x_y[4][7])
 class Agent:
     i = 1
