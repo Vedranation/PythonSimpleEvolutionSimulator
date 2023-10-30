198152d4a1221db04e28653b34511d0b21046153
Vedran <carobnjak.best@gmail.com>
2023-10-30 03:44:42 +0000

    removed extra world types list and made everything use World_agent_list.type

--- venv/main_script.py
+++ venv/main_script.py
@@ -11,8 +11,7 @@ if pow(world_size, 2) < Num_cow + Num_dandelion + Num_tiger: #Check if world is 
     raise Exception("World can't be smaller than amount of objects to spawn")
 
 #Generate the world X in Y, filled with None to show empty cells, starts from XY = 0, ends at at world_size - 1
-World_type_list_x_y = [[None for _ in range(world_size)] for _ in range(world_size)] #stores types of Agents (plant, herbivore, carnivore...)
-World_agent_list_x_y = [[None for _ in range(world_size)] for _ in range(world_size)] #stores specific agent instances
+World_agent_list_x_y = [[None for _ in range(world_size)] for _ in range(world_size)] #stores all agent instances
 class Agent:
     def __init__(self, name, type, perception, speed):
 
@@ -38,10 +37,9 @@ class Agent:
         self.y = random.randint(0, world_size-1)
 
         #Loop until a free spot is found
-        while World_type_list_x_y[self.x][self.y] != None:
+        while World_agent_list_x_y[self.x][self.y] != None:
             self.x = random.randint(0, world_size-1)
             self.y = random.randint(0, world_size-1)
-        World_type_list_x_y[self.x][self.y] = self.type
         World_agent_list_x_y[self.x][self.y] = self
 
     @staticmethod
@@ -49,7 +47,6 @@ class Agent:
         """
         Removes an agent from both the type list and agent list.
         """
-        World_type_list_x_y[agent.x][agent.y] = None
         World_agent_list_x_y[agent.x][agent.y] = None
 
         # Remove from respective list
@@ -72,10 +69,17 @@ class Agent:
             for direction in directions_x_y:
                 if direction[0] >= world_size or direction[1] >= world_size or direction[0] < 0 or direction[1] < 0: #prevents checking beyond edge of world
                     continue
-                print(f"{self.name} X: {self.x} Y: {self.y}  |  check X: {direction[0]} Y:{direction[1]}  |  found: {World_type_list_x_y[direction[0]][direction[1]]}") #console log
+                empty = False
+                if World_agent_list_x_y[direction[0]][direction[1]] == None:
+                    empty = True
+                    print(f"{self.name} X: {self.x} Y: {self.y}  |  check X: {direction[0]} Y:{direction[1]}  |  found: None") #console log
+                    continue
+                else:
+                    print(f"{self.name} X: {self.x} Y: {self.y}  |  check X: {direction[0]} Y:{direction[1]}  |  found: {World_agent_list_x_y[direction[0]][direction[1]].name}")
 
                 #if we found food, eat it and go there:
-                if World_type_list_x_y[direction[0]][direction[1]] == self.food:
+
+                if World_agent_list_x_y[direction[0]][direction[1]].type == self.food:
                     print(f"Found food! Food is: {World_agent_list_x_y[direction[0]][direction[1]].name}") #Find specific instance to eat
 
                     if World_agent_list_x_y[direction[0]][direction[1]].type == "Plant": #if we just ate a plant:
@@ -83,8 +87,6 @@ class Agent:
                     else:
                         Agent.RemoveAgent(World_agent_list_x_y[direction[0]][direction[1]])  # for the agent being eaten
 
-                    World_type_list_x_y[direction[0]][direction[1]] = self.type
-                    World_type_list_x_y[self.x][self.y] = None
                     World_agent_list_x_y[self.x][self.y] = None
                     World_agent_list_x_y[direction[0]][direction[1]] = self
                     self.x = direction[0]
@@ -115,7 +117,7 @@ for i in Dandelion_list:
     print(f"Dandelions are at: X: {i.x} Y: {i.y}")
 for i in Tigers_list:
     print(f"Tigers are at: X: {i.x} Y: {i.y}")
-
+print(f"World started with {Num_dandelion} Dandelions, {Num_cow} Cows, and {Num_tiger} Tigers")
 #simulate Simulation_Length turns (main loop)
 for i in range(Simulation_Length):
     print(f"\nTurn {i+1}\n")
@@ -125,3 +127,5 @@ for i in range(Simulation_Length):
     for tigers in Tigers_list:
         tigers.SearchForFood()
 
+#report results
+print(f"World ended with {len(Dandelion_list)} Dandelions, {len(Cows_list)} Cows, and {len(Tigers_list)} Tigers")
