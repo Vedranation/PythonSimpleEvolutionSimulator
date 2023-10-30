fc517966aa985dc3b3a8d6038f030bd8dd8b27c9
Vedran <carobnjak.best@gmail.com>
2023-10-30 03:07:23 +0000

    Added ability to eat and delete eaten animals

--- venv/main_script.py
+++ venv/main_script.py
@@ -2,11 +2,17 @@
 import random
 
 world_size = 20 #how big (box) do you want world to be
+Simulation_Length = 2 #how many turns in simulation
+Num_dandelion = 10 #how many of each agents do you want
+Num_cow = 10
+Num_tiger = 10
 
+if pow(world_size, 2) < Num_cow + Num_dandelion + Num_tiger: #Check if world is big enough for all agents
+    raise Exception("World can't be smaller than amount of objects to spawn")
 
 #Generate the world X in Y, filled with None to show empty cells, starts from XY = 0, ends at at world_size - 1
-World_list_x_y = [[None for _ in range(world_size)] for _ in range(world_size)]
-
+World_type_list_x_y = [[None for _ in range(world_size)] for _ in range(world_size)] #stores types of Agents (plant, herbivore, carnivore...)
+World_agent_list_x_y = [[None for _ in range(world_size)] for _ in range(world_size)] #stores specific agent instances
 class Agent:
     def __init__(self, name, type, perception, speed):
 
@@ -32,11 +38,27 @@ class Agent:
         self.y = random.randint(0, world_size-1)
 
         #Loop until a free spot is found
-        while World_list_x_y[self.x][self.y] != None:
+        while World_type_list_x_y[self.x][self.y] != None:
             self.x = random.randint(0, world_size-1)
             self.y = random.randint(0, world_size-1)
-        World_list_x_y[self.x][self.y] = self.type
-        return self.x, self.y
+        World_type_list_x_y[self.x][self.y] = self.type
+        World_agent_list_x_y[self.x][self.y] = self
+
+    @staticmethod
+    def RemoveAgent(agent):
+        """
+        Removes an agent from both the type list and agent list.
+        """
+        World_type_list_x_y[agent.x][agent.y] = None
+        World_agent_list_x_y[agent.x][agent.y] = None
+
+        # Remove from respective list
+        if agent.name == "Cow":
+            Cows_list.remove(agent)
+        elif agent.name == "Tiger":
+            Tigers_list.remove(agent)
+        # No need to remove plants from a list because they get respawned
+
 
     def SearchForFood(self):
         directions_x_y = []
@@ -50,26 +72,30 @@ class Agent:
             for direction in directions_x_y:
                 if direction[0] >= world_size or direction[1] >= world_size or direction[0] < 0 or direction[1] < 0: #prevents checking beyond edge of world
                     continue
-                print(f"{self.name} X: {self.x} Y: {self.y}  |  check X: {direction[0]} Y:{direction[1]}  |  found: {World_list_x_y[direction[0]][direction[1]]}") #console log
+                print(f"{self.name} X: {self.x} Y: {self.y}  |  check X: {direction[0]} Y:{direction[1]}  |  found: {World_type_list_x_y[direction[0]][direction[1]]}") #console log
 
-                #if we found food:
-                if World_list_x_y[direction[0]][direction[1]] == self.food:
-                    #World_list_x_y[direction[0]][direction[1]] =
-                    print("Found food!")
+                #if we found food, eat it and go there:
+                if World_type_list_x_y[direction[0]][direction[1]] == self.food:
+                    print(f"Found food! Food is: {World_agent_list_x_y[direction[0]][direction[1]].name}") #Find specific instance to eat
+
+                    if World_agent_list_x_y[direction[0]][direction[1]].type == "Plant": #if we just ate a plant:
+                        World_agent_list_x_y[direction[0]][direction[1]].FindFreeSpot() #respawn plant
+                    else:
+                        Agent.RemoveAgent(World_agent_list_x_y[direction[0]][direction[1]])  # for the agent being eaten
+
+                    World_type_list_x_y[direction[0]][direction[1]] = self.type
+                    World_type_list_x_y[self.x][self.y] = None
+                    World_agent_list_x_y[self.x][self.y] = None
+                    World_agent_list_x_y[direction[0]][direction[1]] = self
+                    self.x = direction[0]
+                    self.y = direction[1]
+
                     break #end the search
 
 
-Simulation_Length = 2 #how many turns in simulation
-Num_dandelion = 10 #how many of each agents do you want
-Num_cow = 5
-Num_tiger = 5
-Cows_list = []
+Cows_list = [] #initialise lists to store agents
 Dandelion_list = []
 Tigers_list = []
-
-
-if pow(world_size, 2) < Num_cow + Num_dandelion: #Check if world is big enough
-    raise Exception("World can't be smaller than amount of objects to spawn")
 
 #spawn agents
 for i in range(Num_dandelion):
@@ -82,6 +108,7 @@ for i in range(Num_tiger):
     Tiger = Agent("Tiger", "Carnivore", 1, 1)
     Tigers_list.append(Tiger)
 
+#console log
 for i in Cows_list:
     print(f"Cows are at: X: {i.x} Y: {i.y}")
 for i in Dandelion_list:
@@ -89,7 +116,7 @@ for i in Dandelion_list:
 for i in Tigers_list:
     print(f"Tigers are at: X: {i.x} Y: {i.y}")
 
-#simulate Simulation_Length turns
+#simulate Simulation_Length turns (main loop)
 for i in range(Simulation_Length):
     print(f"\nTurn {i+1}\n")
     for cows in Cows_list:
@@ -97,3 +124,4 @@ for i in range(Simulation_Length):
     print("\n")
     for tigers in Tigers_list:
         tigers.SearchForFood()
+
